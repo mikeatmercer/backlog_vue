f07eedd10d7c6919c838bfe6118e6fdd0feb460f
mike moore <mike.moore1@mercer.com>
2019-02-01 11:34:12 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="backlog_app">
+      <h1>Mercer Link Product Backlog</h1>
+        <nav-list :activeTab="activeTab" :tabswitch="activeTabSwitch"/>
+    </div>
+
+</template>
+
+<script>
+    import NavList from './Nav.vue';
+    export default {
+      data() {
+        return {
+          activeTab: 'current'
+        }
+      },
+      components: {
+        "nav-list": NavList
+      },
+      methods: {
+        activeTabSwitch(state) {
+          this.activeTab = state;
+        }
+      }
+    }
+</script>
+
+
+<style lang="scss">
+
+  #backlog_app {
+    font-family: "Segoe UI",Arial,sans-serif;
+    width: 710px;
+    h1,h2,h3,h4,h5,h6,p, ul,li, ol, a {
+      font-family: "Segoe UI",Arial,sans-serif !important;
+    }
+  }
+
+</style>
---
+++ src/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="nav-tabs">
+    <button v-for="tab in tabs" :key="tab.state" :class="{active: (activeTab == tab.state) }" v-on:click.prevent="tabswitch(tab.state)">
+      {{tab.title}} --- {{activeTab}}
+    </button>
+
+
+  </div>
+
+</template>
+<script>
+export default {
+  data () {
+    return {
+      nav: 'nav',
+      tabs: [
+        {
+          state: 'current',
+          title: 'Current Backlog Items'
+        },
+        {
+          state: 'completed',
+          title: 'Completed Backlog Items'
+        }
+      ]
+
+    }
+  },
+  props: ['activeTab','tabswitch']
+}
+</script>
+
+<style scoped lang="scss">
+  .nav-tabs {
+    .active {
+      color: red;
+    }
+  }
+
+</style>
